a29ababde1bafbe77257f2a4658dbb2399f5e493
takejir <114567188+takejir@users.noreply.github.com>
2023-06-27 20:57:12 +0900

    Update env.php

--- env.php
+++ env.php
@@ -1,7 +1,7 @@
 <?php
 define('DB_HOST', 'localhost');
 define('DB_NAME', 'blog_app');
-define('DB_USER', 'furisuku');
-define('DB_PASS', 'fu3ri&5%hu$');
+define('DB_USER', 'ユーザー');
+define('DB_PASS', 'パスワード');
 
 ?>
